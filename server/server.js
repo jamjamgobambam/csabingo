e80c647190553c2d5085ddc22fdf89fbeb290b27
jamjamgobambam <jamila.khawaja85@gmail.com>
2023-05-19 15:57:45 -0400

    fix favicon

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const favicon = require('serve-favicon')
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -10,10 +11,10 @@ app.use(express.json())
 app.use('/terms', router)
 
 if (process.env.NODE_ENV === 'development') {
-    app.use(favicon(path.resolve('../', 'client', 'public', 'hollowknight.png')))
+    app.use(favicon(path.resolve('../', 'client', 'public', 'java.png')))
 }
 else if (process.env.NODE_ENV === 'production') {
-    app.use(favicon(path.resolve('public', 'hollowknight.png')))
+    app.use(favicon(path.resolve('public', 'java.png')))
     app.use(express.static('public'))
 }
 
